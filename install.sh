246b9783d9a09243c5c287562541b238ed6a320a
Ajith Sampath <55175424+ajithkathir@users.noreply.github.com>
2024-10-24 09:47:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,14 @@ if ! command -v python3 &> /dev/null; then
     exit 1
 fi
 
+if command -v python3 &>/dev/null; then
+    PYTHON_CMD=python3
+elif command -v python &>/dev/null; then
+    PYTHON_CMD=python
+
 # Create a virtual environment
 echo "Creating virtual environment..."
-python3 -m venv venv
+$PYTHON_CMD -m venv venv
 
 
 # On Windows
@@ -24,7 +29,7 @@ pip install numpy matplotlib scipy pandas healpy astropy casatools casatasks ipy
 
 
 # Add the virtual environment as a new Jupyter kernel
-python3 -m ipykernel install --user --name=venv --display-name "Hirax Python env"
+$PYTHON_CMD -m ipykernel install --user --name=venv --display-name "Hirax Python env"
 
 
 # Verification of the installation
